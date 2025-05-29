ffec1cb272ced7649be01168dfb18172a2a74bda
Fariha Promi <farihapromi2611@gmail.com>
2025-05-29 19:55:12 +0600

    Add signUp

--- job-portal-client/src/Routers/Router.jsx
+++ job-portal-client/src/Routers/Router.jsx
@@ -3,10 +3,23 @@ import {
  
 } from "react-router-dom";
 import MainLayout from '../layout/MainLayout';
+import Home from '../Pages/Home/Home';
+import Signup from '../Pages/SignUp/Signup';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout/>,
+   errorElement:<h2>Route Not found</h2>,
+    children:[
+        {
+            path:'/',
+            element:<Home/>
+        },
+        {
+            path:'/sign-up',
+            element:<Signup/>
+        }
+    ]
   },
 ]);
 export default router
--- job-portal-client/src/layout/MainLayout.jsx
+++ job-portal-client/src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../Pages/Home/Shared/Navbar'
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
+        <Navbar/>
         <Outlet/>
       
     </div>
