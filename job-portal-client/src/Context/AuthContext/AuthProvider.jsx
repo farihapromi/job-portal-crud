f82e700b117a89272a3c84f5aedd043e4dc915d7
Fariha Promi <farihapromi2611@gmail.com>
2025-05-30 11:08:22 +0600

    Add AuthProvider

---
+++ job-portal-client/src/Context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import AuthContext from './AuthContext'
+
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState(null)
+    const [loading,setLoading]=useState(true)
+    const userInfo={
+        user,
+        loading
+
+    }
+  return (
+    <AuthContext.Provider value={userInfo}>
+       {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
