c418bef1f96f30a1b7add6db81b2ed325bb1c498
Fariha Promi <farihapromi2611@gmail.com>
2025-05-30 10:57:44 +0600

    Add context

--- job-portal-client/src/Pages/SignUp/Signup.jsx
+++ job-portal-client/src/Pages/SignUp/Signup.jsx
@@ -4,28 +4,35 @@ import { Link } from 'react-router-dom'
 import registerAnimationData from '../../assets/lottie/register.json'
 
 const Signup = () => {
+  const handleSignup=e=>{
+    e.preventDefault();
+    const form=e.target;
+    const email=form.email.value;
+    const password=form.password.value;
+    console.log(email,password)
+  }
   return (
     <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
+    <div className="text-center lg:text-left w-96">
         <Lottie animationData={registerAnimationData }></Lottie>
      
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
          <h1 className=" ml-8 mt-4 text-5xl font-bold">Signup now!</h1>
      
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleSignup}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" name ="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="password" name="password"className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
