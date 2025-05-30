2612618e48aba0b583bd8d22e919db29d5dd686b
Fariha Promi <farihapromi2611@gmail.com>
2025-05-30 21:57:19 +0600

    update index file

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${MONGO_PASS}@cluster0.u7yariw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.u7yariw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
