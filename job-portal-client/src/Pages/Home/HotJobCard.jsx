1abdff7644c1f4a1c0224ba354d3c3e5833890f6
Fariha Promi <farihapromi2611@gmail.com>
2025-05-31 11:12:24 +0600

    Add jobcard

---
+++ job-portal-client/src/Pages/Home/HotJobCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { LiaMapMarkerAltSolid } from "react-icons/lia";
+
+const HotJobCard = ({job}) => {
+  const { title, location, jobType, category, applicationDeadline, salaryRange, description, company, requirements, responsibilities, status, hr_email, hr_name, company_logo } = job;
+
+  return (
+    <div className="card card-compact bg-base-100  shadow-xl">
+ <div className='flex gap-2 m-4'>
+     <figure>
+    <img
+    className='w-16'
+    src={company_logo}
+      alt="logo" />
+  </figure>
+  <div>
+    <h1 className="text-2xl">{company}</h1>
+    <p className='flex gap-2 items-center'> <LiaMapMarkerAltSolid/>{location}</p>
+  </div>
+ </div>
+  <div className="card-body">
+    <h2 className="card-title">{title}</h2>
+         <div className="badge badge-secondary">NEW</div>
+    <p>{description}</p>
+    <div className='flex gap-2 flex-wrap'>
+        {
+            requirements.map(skill=><p className='border rounded-md text-center px-2 hover:text-white hover:bg-blue-700'>{skill}</p>)
+        }
+    </div>
+    <div className="card-actions justify-end items-center mt-4">
+        <p>Salary :{salaryRange.min}-{salaryRange.max} {salaryRange.currency}</p>
+      <button className="btn bg-gray-300 text-blue-500 hover:bg-blue-600 hover:text-white rounded-lg">Apply</button>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default HotJobCard
--- job-portal-client/src/Pages/Home/HotJobs.jsx
+++ job-portal-client/src/Pages/Home/HotJobs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import HotJobCard from './HotJobCard'
 
 const HotJobs = () => {
     const [jobs,setJobs]=useState([])
@@ -13,8 +14,14 @@ const HotJobs = () => {
     },[])
   return (
     <div>
+        <div className='grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {
+            jobs.map(job=><HotJobCard key={job._id} job={job}></HotJobCard>)
+        }
       
     </div>
+    </div>
+
   )
 }
 
