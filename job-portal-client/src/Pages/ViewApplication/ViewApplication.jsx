f1083db2ed8a840fc3d831e7c92dde62a04bbd45
Fariha Promi <farihapromi2611@gmail.com>
2025-06-01 23:55:47 +0600

    Add status chnage

--- job-portal-client/src/Pages/ViewApplication/ViewApplication.jsx
+++ job-portal-client/src/Pages/ViewApplication/ViewApplication.jsx
@@ -4,6 +4,27 @@ import { useLoaderData } from 'react-router-dom'
 const ViewApplication = () => {
     const applications=useLoaderData()
     console.log(applications)
+    const handleStatusUpdate=(e,id)=>{
+        console.log(e.target.value,id)
+        const data={
+            status:e.target.value
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`,{
+            method:'PATCH',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+
+            
+        })
+        .catch(error=>console.log(error))
+
+    }
   return (
     <div>
       <h3 className="text-2xl">View Applications {applications.length}</h3>
@@ -15,8 +36,8 @@ const ViewApplication = () => {
       <tr>
         <th></th>
         <th>Email</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Status</th>
+        <th>Update Status</th>
       </tr>
     </thead>
     <tbody>
@@ -24,6 +45,17 @@ const ViewApplication = () => {
         applications.map((app,index)=><tr key={app._id}>
             <th>{index+1}</th>
             <td>{app.applicant_email}</td>
+            <td>
+                <select
+                onChange={(e)=>handleStatusUpdate(e,app._id)}
+                defaultValue={app.status||'Changed Status'}
+                 className="select select-bordered select-xs w-full max-w-xs">
+  <option disabled selected>Under Review</option>
+  <option>Set interview</option>
+  <option>Hired</option>
+  <option>Rejected</option>
+</select>
+            </td>
         </tr>)
      }
      
--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -77,6 +77,21 @@ async function run() {
       const result = await jobApplicationCollection.find(query).toArray();
       res.send(result);
     });
+    app.patch('/job-applications/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await jobApplicationCollection.updateOne(
+        filter,
+        updateDoc
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
