80b1829dbe72e6e827551ce4b85c626c664364b6
Fariha Promi <farihapromi2611@gmail.com>
2025-06-04 12:18:53 +0600

    Add jwt

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 
@@ -24,6 +25,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //auth related api,jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, 'secret', { expiresIn: '1h' });
+      res.send(token);
+    });
 
     //job related API
     const jobCollection = client.db('jobPortal').collection('jobs');
