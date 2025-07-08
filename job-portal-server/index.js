916dfb65a4aa0bdf3e101c6e0b66950c72b47dff
Fariha Promi <farihapromi2611@gmail.com>
2025-07-08 10:28:01 +0600

    Add cors

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://job-portal-crud-pi.vercel.app/'],
     credentials: true,
   })
 );
