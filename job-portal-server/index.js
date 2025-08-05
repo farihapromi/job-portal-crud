0e4235fa943df050a262c5342dd2f5922764ab87
Fariha Promi <farihapromi2611@gmail.com>
2025-08-05 21:32:14 +0600

    Fix jwt issues

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -59,10 +59,12 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '5h',
       });
+
       res
         .cookie('token', token, {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         })
         .send({ success: true });
     });
